d7797d265758e6dd17e63706baecba0ab8caa3e3
Sajal <sajalzaki@gmail.com>
2016-06-10 12:28:44 -0400

    new change

--- script.js
+++ script.js
@@ -3,20 +3,7 @@ $(document).ready(function() {//so page is loaded fully and safe for us to manip
    $("#search").on("submit", function(e) {
        e.preventDefault();
 
-       // $.getJSON("http://www.omdbapi.com/?", {
-       //         t: $('input[name=t]').val(),
-       //     },
-       //    function(response) {
-
-
-       //         $("#movie-title").html(response.Title);
-       //         $("#poster").attr("src", response.Poster);
-               
-       //        $("#actors").html("Actors: " + response.Actors);
-       //       $("#directors").html("Directors: " + response.Director);
-        
-       //         console.log(response);
-       //     });
+      
       var dataIN = {t: $('input[name=t]').val()};
        $.ajax({
         type: "GET",
