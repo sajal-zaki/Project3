2cf90a46df5022e3818c69efd3c93339d6573c59
Jack Zhang <jackzhng528@gmail.com>
2016-06-10 11:06:51 -0400

    new changes

---
+++ script.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {//so page is loaded fully and safe for us to manipulate
+
+   $("#search").on("submit", function(e) {
+       e.preventDefault();
+
+       // $.getJSON("http://www.omdbapi.com/?", {
+       //         t: $('input[name=t]').val(),
+       //     },
+       //    function(response) {
+
+
+       //         $("#movie-title").html(response.Title);
+       //         $("#poster").attr("src", response.Poster);
+               
+       //        $("#actors").html("Actors: " + response.Actors);
+       //       $("#directors").html("Directors: " + response.Director);
+        
+       //         console.log(response);
+       //     });
+      var dataIN = {t: $('input[name=t]').val()};
+       $.ajax({
+        type: "GET",
+        url: 'http://www.omdbapi.com/?',
+        data: dataIN,
+        success: function(response) {
+
+               $("#movie-title").html(response.Title);
+               $("#poster").attr("src", response.Poster);
+               
+              $("#actors").html("Actors: " + response.Actors);
+             $("#directors").html("Directors: " + response.Director);
+             console.log(response);
+        }
+       })
+   })
+});
+
+
+function onKeyUp() {
+
+    // $.ajax({
+    //     type: "GET",
+    //     url: 'http://www.omdbapi.com/?',
+    //     data: {t: $('input[name=t]').val(),
+    //     },
+    //     success: function(response){
+
+    //            $("#movie-title").html(response.Title);
+    //            $("#poster").attr("src", response.Poster);
+               
+    //          //  $("#actors").html("Actors: " + response.Actors);
+    //          // $("#directors").html("Directors: " + response.Director);
+    //          console.log(response);
+    //     }
+    //    })
+  
+     $.getJSON("http://www.omdbapi.com/?", {
+               t: $('input[name=t]').val(),
+           },
+          function(response) {
+               $("#movie-title").html(response.Title);
+               $("#poster").attr("src", response.Poster);
+             
+               console.log(response);
+               console.log(response.Actors)
+           });;
+}
